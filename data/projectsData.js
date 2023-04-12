57c37e8373f10f68c73169ffba07b6375c0443c3
Yullu <joshuamuuo5@gmail.com>
2023-04-12 15:36:55 +0300

    shorter description

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,10 +1,7 @@
 const projectsData = [
   {
     title: 'Podcast : @TheeSafeHouse',
-    description: `Welcome to TheeSafeHouse - the place where we hide from life's chaos and take care of our mental health, one meme at a time. As Kenyan youths, we know that life can sometimes feel like a never-ending game of dodging potholes on a Nairobi road. But fear not, dear friends, for within these virtual walls, we've got your mental health covered like a matatu tout haggling for the last seat.
-Here, we don't just talk about our problems, we meme about them. We laugh in the face of anxiety and depression because sometimes, that's the best way to show them who's boss. So if you're feeling a bit down, come on in and take a load off. We've got good vibes, bad jokes, and a whole lot of understanding.
-In the Safe House, we're not afraid to tackle tough topics like mental health and societal pressures head-on. But don't worry, we'll do it in a way that won't make you want to run for the hills. We may not have all the answers, but we've got a pretty solid stash of relatable memes and witty one-liners to get you through even the toughest of days.
-So join us in the Safe House, where mental health and humor go hand in hand. Because life may be tough, but we're tougher. And we've got a whole lot of memes to prove it.`,
+    description: `The Safe House is a virtual space for Kenyan youths to discuss mental health and life. They use humor and memes to address tough topics like anxiety and depression. The Safe House offers good vibes, relatable content, and a supportive community for those seeking a mental health break.`,
     imgSrc: '/static/images/tshpodcast.jpg',
     href: 'https://open.spotify.com/show/4NtWu30NuKCmEVj49hw3TW?si=89404cf13ca347ad',
   },
