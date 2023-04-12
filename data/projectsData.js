1e7fb733e93684c0ecd465823e0b37bede27a8a0
Yullu <joshuamuuo5@gmail.com>
2023-04-12 15:35:30 +0300

    new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,12 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Podcast : @TheeSafeHouse',
+    description: `Welcome to TheeSafeHouse - the place where we hide from life's chaos and take care of our mental health, one meme at a time. As Kenyan youths, we know that life can sometimes feel like a never-ending game of dodging potholes on a Nairobi road. But fear not, dear friends, for within these virtual walls, we've got your mental health covered like a matatu tout haggling for the last seat.
+Here, we don't just talk about our problems, we meme about them. We laugh in the face of anxiety and depression because sometimes, that's the best way to show them who's boss. So if you're feeling a bit down, come on in and take a load off. We've got good vibes, bad jokes, and a whole lot of understanding.
+In the Safe House, we're not afraid to tackle tough topics like mental health and societal pressures head-on. But don't worry, we'll do it in a way that won't make you want to run for the hills. We may not have all the answers, but we've got a pretty solid stash of relatable memes and witty one-liners to get you through even the toughest of days.
+So join us in the Safe House, where mental health and humor go hand in hand. Because life may be tough, but we're tougher. And we've got a whole lot of memes to prove it.`,
+    imgSrc: '/static/images/tshpodcast.jpg',
+    href: 'https://open.spotify.com/show/4NtWu30NuKCmEVj49hw3TW?si=89404cf13ca347ad',
   },
 ]
 
